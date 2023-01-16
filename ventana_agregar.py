0b14161b668cbf81cb6e56894515f9f8fa74a600
mo3bius <ruben.sierra03@gmail.com>
2023-01-16 18:07:09 +0100

    Implemented mostrar_df()

--- ventana_agregar.py
+++ ventana_agregar.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import tkinter as tk
+from tkinter import ttk
 
 def cargar_archivo():
     # Cargar el archivo excel en el dataframe
@@ -23,6 +24,19 @@ def agregar_producto_excel(nombre,stock,precio):
     df_arch = pd.concat([df_arch, df_producto])
     print(df_arch)
     df_arch.to_excel("inventario.xlsx",index=False)
+
+def mostrar_df():
+    df = cargar_archivo()
+    ventana_df = tk.Toplevel()
+    ventana_df.title("Dataframe")
+    ventana_df.geometry("600x400")
+    ventana_df.protocol("WM_DELETE_WINDOW", ventana_df.destroy)
+    frame_df = ttk.Frame(ventana_df)
+    frame_df.pack(expand=True, fill='both')
+    tabla_df = ttk.Label(frame_df, text=df.to_string())
+    tabla_df.pack(expand=True, fill='both')
+    boton_cerrar = ttk.Button(ventana_df, text="Cerrar", command=ventana_df.destroy)
+    boton_cerrar.pack()
     
 def limpiar():
     nombre_entry.delete(0, "end")
@@ -55,8 +69,11 @@ precio_label.grid(row=3, column=0)
 precio_entry = tk.Entry(label_agregar_frame)
 precio_entry.grid(row=3, column=1)
 
-boton_agregar = tk.Button(label_agregar_frame, text="Añadir", command=lambda: [agregar_producto_excel(nombre_entry.get(), stock_entry.get(), precio_entry.get()) ,limpiar()])
+boton_agregar = tk.Button(label_agregar_frame, text="Agregar", command=lambda: [agregar_producto_excel(nombre_entry.get(), stock_entry.get(), precio_entry.get()) ,limpiar()])
 boton_agregar.grid(row=4, column=1)
+
+boton_mostrar = tk.Button(label_agregar_frame, text="Mostrar", command=lambda: mostrar_df())
+boton_mostrar.grid(row=4, column=0)
 
 boton_salir = tk.Button(label_agregar_frame, text="Volver", command=root.destroy)
 boton_salir.grid(row=4, column=2)
