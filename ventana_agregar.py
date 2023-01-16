3ba3f30238c382e08fe5a42b1b0bcc9d2e3b282d
Alf-caput <alfre.roble.aba@gmail.com>
2023-01-16 19:05:59 +0100

    readeable

--- ventana_agregar.py
+++ ventana_agregar.py
@@ -2,14 +2,26 @@ import pandas as pd
 import tkinter as tk
 from tkinter import ttk
 
+
+def limpiar_entradas(*entradas):
+    for entrada in entradas:
+        entrada.delete(0, 'end')
+    return
+
+
+def obtener_caracteristicas(*entradas):
+    return [entrada.get() for entrada in entradas]
+
+
 def cargar_archivo():
     # Cargar el archivo excel en el dataframe
     try:
         df_arch = pd.read_excel('inventario.xlsx')
     except FileNotFoundError:
         df_arch = pd.DataFrame(columns=['Nombre', 'Stock', 'Precio'])
-        df_arch.to_excel('inventario.xlsx',index=False)
+        df_arch.to_excel('inventario.xlsx', index=False)
     return df_arch
+
 
 def agregar_producto(nombre, stock, precio):
     # Función para agregar el producto al diccionario de inventario
@@ -36,40 +48,51 @@ def mostrar_df():
     tabla_df.pack(expand=True, fill='both')
     boton_cerrar = ttk.Button(ventana_df, text="Cerrar", command=ventana_df.destroy)
     boton_cerrar.pack()
-    
-root = tk.Tk()
-root.title("Agregar producto")
-# root.geometry("400x300")
 
-frame_agregar = tk.Frame(root)
-frame_agregar.pack()
-label_agregar_frame = tk.LabelFrame(frame_agregar, text="Introduzca el producto:")
-label_agregar_frame.config(bg="burlywood1")
-label_agregar_frame.grid(row=0 ,column=0)
 
-# Creación de etiquetas y cajas de texto para ingresar los datos del producto
-nombre_label = tk.Label(label_agregar_frame, text="Nombre:", bg="burlywood1")
-nombre_label.grid(row=1, column=0)
-nombre_entry = tk.Entry(label_agregar_frame)
-nombre_entry.grid(row=1, column=1)
+def interfaz_agregar():
+    root = tk.Tk()
+    root.title("Agregar producto")
+    root.resizable(False, False)
+    # root.geometry("400x300")
 
-stock_label = tk.Label(label_agregar_frame, text="Stock:", bg="burlywood1")
-stock_label.grid(row=2, column=0)
-stock_entry = tk.Entry(label_agregar_frame)
-stock_entry.grid(row=2, column=1)
+    ventana_escritura = tk.Frame(root)
+    ventana_escritura.pack()
 
-precio_label = tk.Label(label_agregar_frame, text="Precio:", bg="burlywood1")
-precio_label.grid(row=3, column=0)
-precio_entry = tk.Entry(label_agregar_frame)
-precio_entry.grid(row=3, column=1)
+    frame_escritura = tk.LabelFrame(ventana_escritura, text="Introduzca el producto:")
+    frame_escritura.config(bg="burlywood1")
+    frame_escritura.grid(row=0, column=0)
 
-boton_agregar = tk.Button(label_agregar_frame, text="Agregar", command=lambda: [agregar_producto_excel(nombre_entry.get(), stock_entry.get(), precio_entry.get()) , nombre_entry.delete(0, "end"), stock_entry.delete(0, "end"), precio_entry.delete(0, "end")])
-boton_agregar.grid(row=4, column=1)
+    etiqueta_nombre = tk.Label(frame_escritura, text="Nombre:", bg="burlywood1")
+    entrada_nombre = tk.Entry(frame_escritura)
+    etiqueta_stock = tk.Label(frame_escritura, text="Stock:", bg="burlywood1")
+    entrada_stock = tk.Entry(frame_escritura)
+    etiqueta_precio = tk.Label(frame_escritura, text="Precio:", bg="burlywood1")
+    entrada_precio = tk.Entry(frame_escritura)
 
-boton_mostrar = tk.Button(label_agregar_frame, text="Mostrar", command=lambda: mostrar_df())
-boton_mostrar.grid(row=4, column=0)
+    objetos = [etiqueta_nombre, entrada_nombre, etiqueta_stock, entrada_stock, etiqueta_precio, entrada_precio]
 
-boton_salir = tk.Button(label_agregar_frame, text="Volver", command=root.destroy)
-boton_salir.grid(row=4, column=2)
+    for idx, objeto in enumerate(objetos):
+        objeto.grid(row=idx // 2, column=idx % 2, sticky="news", padx=20, pady=10)
 
-root.mainloop()
+    frame_accion = tk.LabelFrame(ventana_escritura)
+    frame_accion.config(bg="burlywood1")
+    frame_accion.grid(row=1, column=0)
+
+    # Creación de etiquetas y cajas de texto para ingresar los datos del producto
+
+    boton_agregar = tk.Button(frame_accion, text="Agregar", command=lambda: (agregar_producto_excel(*obtener_caracteristicas(entrada_nombre, entrada_stock, entrada_precio)),
+                                                                             limpiar_entradas(entrada_nombre, entrada_stock, entrada_precio)))
+    boton_mostrar = tk.Button(frame_accion, text="Mostrar", command=lambda: mostrar_df())
+    boton_salir = tk.Button(frame_accion, text="Volver", command=root.destroy)
+
+    botones = [boton_agregar, boton_mostrar, boton_salir]
+
+    for idx, boton in enumerate(botones):
+        boton.grid(row=0, column=idx, sticky="news", padx=20, pady=10)
+
+    root.mainloop()
+    return
+
+
+interfaz_agregar()
