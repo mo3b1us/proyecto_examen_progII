b12ed9ade9bf942edadeed3657f9aee4433a9da5
mo3bius <ruben.sierra03@gmail.com>
2023-01-16 20:34:47 +0100

    dont know if it works

---
+++ modificar.py
@@ -0,0 +1,40 @@
+import ventana_agregar.py
+import pandas as pd
+import tkinter as tk
+
+def modificar(producto):
+    # Crear la ventana de tkinter
+    ventana_modificar = tk.Tk()
+    ventana_modificar.title("Modificar producto: " + producto)
+
+    # Crear las etiquetas y los cuadros de texto para ingresar los nuevos valores
+    nombre_label = tk.Label(ventana_modificar, text="Nombre:")
+    nombre_label.grid(row=0, column=0)
+    nombre_entry = tk.Entry(ventana_modificar)
+    nombre_entry.grid(row=0, column=1)
+
+    stock_label = tk.Label(ventana_modificar, text="Stock:")
+    stock_label.grid(row=1, column=0)
+    stock_entry = tk.Entry(ventana_modificar)
+    stock_entry.grid(row=1, column=1)
+
+    precio_label = tk.Label(ventana_modificar, text="Precio:")
+    precio_label.grid(row=2, column=0)
+    precio_entry = tk.Entry(ventana_modificar)
+    precio_entry.grid(row=2, column=1)
+
+    # Crear el botón para confirmar la modificación
+    boton_modificar = tk.Button(ventana_modificar, text="Modificar", command=lambda: modificar_producto (producto,nombre_entry.get(), stock_entry.get(), precio_entry.get()))
+    boton_modificar.grid(row=3, column=1)
+    boton_salir = tk.Button(ventana_modificar, text="Volver", command=ventana_modificar.destroy)
+    boton_salir.grid(row=5, column=1)
+    
+    ventana_modificar.mainloop()
+
+def modificar_producto(producto,nombre,stock,precio):
+    df = cargar_archivo()
+    df.loc[df['Nombre'] == producto, 'Nombre'] = nombre
+    df.loc[df['Nombre'] == nombre, 'Stock'] = stock
+    df.loc[df['Nombre'] == nombre, 'Precio'] = precio
+    #sobreescribimos el excel
+    df.to_excel("inventario.xlsx", index=False)
--- ventana_agregar.py
+++ ventana_agregar.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import tkinter as tk
 from tkinter import ttk
-
+from tkintertable import TableCanvas
 
 def limpiar_entradas(*entradas):
     for entrada in entradas:
@@ -39,53 +39,19 @@ def agregar_producto_excel(nombre,stock,precio):
 
 def mostrar_df():
     df = cargar_archivo()
-    ventana_df = tk.Toplevel()
-    ventana_df.title("Dataframe")
-    ventana_df.geometry("600x400")
-    frame_df = ttk.Frame(ventana_df)
-    frame_df.pack(expand=True, fill='both')
-    tabla_df = ttk.Label(frame_df, text=df.to_string())
-    tabla_df.pack(expand=True, fill='both')
+    #ventana_df = tk.Toplevel()
+    #ventana_df.title("Dataframe")
+    #ventana_df.geometry("600x400")
+    #frame_df = ttk.Frame(ventana_df)
+    #frame_df.pack(expand=True, fill='both')
+    #tabla_df = ttk.Label(frame_df, text=df.to_string())
+    #tabla_df.pack(expand=True, fill='both')
+    ventana_df = tk.Tk()
+    table = TableCanvas(ventana_df, dataframe=df)
+    table.show()
     boton_cerrar = ttk.Button(ventana_df, text="Cerrar", command=ventana_df.destroy)
     boton_cerrar.pack()
-
-def modificar(producto):
-    # Crear la ventana de tkinter
-    ventana_modificar = tk.Tk()
-    ventana_modificar.title("Modificar producto: " + producto)
-
-    # Crear las etiquetas y los cuadros de texto para ingresar los nuevos valores
-    nombre_label = tk.Label(ventana_modificar, text="Nombre:")
-    nombre_label.grid(row=0, column=0)
-    nombre_entry = tk.Entry(ventana_modificar)
-    nombre_entry.grid(row=0, column=1)
-
-    stock_label = tk.Label(ventana_modificar, text="Stock:")
-    stock_label.grid(row=1, column=0)
-    stock_entry = tk.Entry(ventana_modificar)
-    stock_entry.grid(row=1, column=1)
-
-    precio_label = tk.Label(ventana_modificar, text="Precio:")
-    precio_label.grid(row=2, column=0)
-    precio_entry = tk.Entry(ventana_modificar)
-    precio_entry.grid(row=2, column=1)
-
-    # Crear el botón para confirmar la modificación
-    boton_modificar = tk.Button(ventana_modificar, text="Modificar", command=lambda: modificar_producto (producto,nombre_entry.get(), stock_entry.get(), precio_entry.get()))
-    boton_modificar.grid(row=3, column=1)
-    boton_salir = tk.Button(ventana_modificar, text="Volver", command=ventana_modificar.destroy)
-    boton_salir.grid(row=5, column=1)
-    
-    ventana_modificar.mainloop()
-
-def modificar_producto(producto,nombre,stock,precio):
-    df = cargar_archivo()
-    df.loc[df['Nombre'] == producto, 'Nombre'] = nombre
-    df.loc[df['Nombre'] == nombre, 'Stock'] = stock
-    df.loc[df['Nombre'] == nombre, 'Precio'] = precio
-    #sobreescribimos el excel
-    df.to_excel("inventario.xlsx", index=False)
-
+    ventana_df.mainloop()
 
 def interfaz_agregar():
     root = tk.Tk()
